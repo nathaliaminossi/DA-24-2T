7b1133119c2e8250d8c8148001af059cb884c194
aline <yagohoc@gmail.com>
2024-08-12 17:22:48 -0300

    aula 3

---
+++ aulas/aula3/index.js
@@ -0,0 +1,43 @@
+console.log ("ola mundo!")
+console.log 
+//prompt("qual o seu nome") 
+                   
+const nome= "nathalia"
+const idade= "16"
+console.log("olá", "meu nome é:", nome, "e eu tenho:", idade, "anos");
+const got= "game of thrones"
+const temporadasDeGot = "8"; 
+
+console.log(typeof got)
+console.log(typeof temporadasDeGot)
+
+
+let novaVariavel
+console.log(typeof novaVariavel)
+
+novaVariavel = 2 
+console.log(typeof novaVariavel)
+
+novaVariavel = undefined
+console.log(typeof novaVariavel)
+
+//const usuario1 = prompt("nome do usario")
+
+//console.log(usuario1)
+
+
+//const nathalia = prompt("idade do usuario")
+
+console.log(16)
+
+const idadeNumero = 23
+const idadetexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+
+const idadeNumero2 = Number (idadeTexto2) 
+console.log(typeof idadeTexto2) 
+console.log(trypeof idadeNumero2)
+
+
+
