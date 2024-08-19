8a2c66a981e979ed03fd5415b0deeb6b8d75d6dc
aline <yagohoc@gmail.com>
2024-08-19 17:13:37 -0300

    aula6

---
+++ aulas/aula 5 variaveis/index.js
@@ -0,0 +1,58 @@
+/*let a = 10
+let b = 10
+//o código define duas variaveis "a e b", as duas com valor de 10 
+console.log (b)
+
+// o console log é imprimirá os valores das variaveis 'a e b' 
+b = 5
+
+console.log (a, b)
+
+//o código altera o valor da variavel 'b' para 5 
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+//todas as variaveis terao o valor final 10. o valor do 'c' é atribuido pelp valor de 'a'
+
+console.log (a, b , c)
+//no console apresenta 10,10,10
+
+
+let p=prompt("quantas horas você trabalha por dia?")
+let t= prompt("quanto você recebe por dia?")
+//o 'p'  informa quantas horas trabalha por dia e o 't' solicita que informe quamtas horas trabalha 
+console.log("você recebe",t,"/",p,"por hora")
+//exibe a quantidade que recebe por hora*/
+
+//let nome
+//let idade
+
+//console.log(typeof idade)
+//console.log(typeof nome)
+// UNDEFINED pois o valor nao foi definido
+
+ //nome = prompt("digite seu nome:");
+//idade = prompt("digite sua idade:");
+ 
+//console.log(typeof idade, typeof nome)
+
+//console.log("olá ",nome," voce tem ",idade,"anos")
+
+let a = 10 
+let b = 25
+
+console.log(a): //25
+console.log(b): //10 
+
+
+
+console.log("o novo valor de a é", a) // o novo valor de a é 25
+console.log("o novo valor de b é", b) // o novo valor de b é 10
+
+
+
+
+
---
+++ aulas/aula6/index.js
@@ -0,0 +1,125 @@
+//aula 6 operaddores aritméticos
+//const primeiroValor = 10
+//const segundoValor = 20 
+//const resultado = primeiroValor + segundoValor + 4
+
+//console.log("o resultado é:",resultado)
+//const primeiroValor = 10
+//const segundoValor = 20
+//const resultado = primeiroValor - segundoValor
+//console.log(resultado)// -10
+/*
+const primeiroValor = 10
+const segundoValor = 25
+const resultado = primeiroValor * segundoValor
+
+console.log(resultado)// 250
+
+*/
+/*
+const primeiroValor = 345
+const segundoValor = 10
+const resultado = primeiroValor/segundoValor
+
+console.log("o resultado é:", resultado)
+*/
+//exemplo resto da divisao
+
+/*
+const restoDivisao = 11%4
+
+console.log("o resto da divisao é:",restoDivisao)
+
+
+/*
+const soma = 3+4;
+console.log("soma:", soma)
+
+/*
+const multiplicacaoDivisao = (3*5)/2;
+console.log("multiplicacaoDivisao", multiplicacaoDivisao);
+
+/*
+const subtracaoMultiplicacao = (4-5)* -1
+
+console.log("subtracao e multiplicacao",subtracaoMultiplicacao)
+
+/*
+const restoDivisao = 234 % 5
+
+console.log("resto da divisao:", restoDivisao)
+
+/*
+const condicao = 1 === "1"
+console.log(condicao)
+//comparaco de valor e tipo igual
+const condicao2 = 1 === "1"
+console.log(condicao2)
+
+*/
+
+//comparador diferente !=
+
+/*
+const condicao = 1 !== "1"
+console.log(condicao)//resultado true
+const condicao2 = 1 !== 1 
+console.log(condicao2)
+const condicao3 = 2 !==1 
+console.log(condicao3)
+
+/*
+//comparador maior e menor
+const condicao = 1 > 2 
+console.log(condicao)
+
+const condicao1 = 2 > 2 
+console.log(condicao1)
+
+const condicao2 = 2 >= 2 
+console.log(condicao2)
+
+const condicao3 = 3 > 2 
+console.log(condicao3)
+/*
+//comparador menor, menor e igual
+const condicao = 1 < 2
+console.log(condicao)
+
+const condicao1 = 2 < 2 
+console.log(condicao1)
+
+const condicao2 = 2 <= 2
+console.log(condicao2)
+
+const condicao3 = 3 < 2 
+console.log(condicao3)
+
+/*
+const numero1 = 4
+const numero2 = 8
+ 
+console.log("o primeiro numero é igual ao segundo?",numero1===numero2)
+/*
+const numero1 = 4
+const numero2 = 8
+
+console.log("o primeiro numero é diferente do segundo?",numero1===numero2)
+*/
+const numero1 = 4 
+const numero2 = 8
+
+console.log("o primeiro numero é maior que o segundo?",numero1>numero2)
+/*
+const numero1 = 4
+const numero2 = 8
+
+console.log("o primeiro numero é menor que o segundo?",numero1=== numero2)
+
+
+
+
+
+
+
+
